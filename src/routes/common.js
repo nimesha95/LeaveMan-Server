0b2b92ea821e1cb2c0573e72bf1f62dc11490e58
nimesha95 <nimesha95@live.com>
2018-06-10 08:56:23 +0530

    created some basic api for user

--- src/routes/common.js
+++ src/routes/common.js
@@ -11,6 +11,11 @@ router.post('/make_leave', authenticate, function (req, res) {
     res.json({user_info: "It works"});
 });
 
+//look tihsi sfasa 
+router.get('/leave_info_summary', function (req, res) {
+    res.json({data: [["Effort", "Amount given"], ["My all", 50],["My all2", 20]]});
+});
+
 router.post('/edit_leave', authenticate, function (req, res) {
     res.json({user_info: "It works"});
 });
